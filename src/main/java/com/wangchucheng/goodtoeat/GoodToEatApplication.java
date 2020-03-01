3fafba456aca84eb069c25045f9295ece7c2bc52
Wang Chucheng <ccwangchn@gmail.com>
2020-03-01 13:09:20 +0800

    Initial commit

---
+++ src/main/java/com/wangchucheng/goodtoeat/GoodToEatApplication.java
@@ -0,0 +1,13 @@
+package com.wangchucheng.goodtoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodToEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodToEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangchucheng/goodtoeat/GoodToEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangchucheng.goodtoeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodToEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
