2986eb507a3848f71e8a0361ffa582754dfeab53
Wang Chucheng <ccwangchn@gmail.com>
2020-04-23 17:29:08 +0800

    refactor: change time format

--- src/main/java/com/wangchucheng/goodtoeat/recipe/Recipe.java
+++ src/main/java/com/wangchucheng/goodtoeat/recipe/Recipe.java
@@ -23,7 +23,7 @@ public class Recipe {
     private String image;//标题图
 
     @Column(nullable = false)
-    private Timestamp posttime;
+    private Timestamp time;
 
     @Column(nullable = false)
     private String timeNeeded;//耗时
@@ -61,22 +61,12 @@ public class Recipe {
         this.id = id;
     }
 
-    public Timestamp getPosttime() {
-        return posttime;
+    public Timestamp getTime() {
+        return time;
     }
 
-    public void setPosttime(Timestamp posttime) {
-        this.posttime = posttime;
-    }
-
-    public Recipe(String openid, String title, String image, String timeNeeded, String difficulty, String size, int collectedTimes){
-        this.openid = openid;
-        this.title=title;
-        this.image=image;
-        this.timeNeeded=timeNeeded;
-        this.difficulty=difficulty;
-        this.size=size;
-        this.collectedTimes=collectedTimes;
+    public void setTime(Timestamp posttime) {
+        this.time = posttime;
     }
 
     public String getIngredients() {
